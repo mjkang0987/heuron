e63c024f2758bcad1aa20f341ccb4cc4a94224d6
Minjeong kang <snowdrop1202@gmail.com>
2021-12-08 23:30:02 +0900

    feat: 헤더 - 태그에 클래스 추가

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,10 +5,10 @@ export const Header = () => {
         <header>
             <div className="header-wrap">
                 <span className="logo"><a href="#" className="link-home">로고</a></span>
-                <nav>
+                <nav className="nav">
                     <ul>
                         {NAV.map((nav, index) => <li key={`nav-${index}`}>
-                            <a href={nav.LINK}>{nav.NAME}</a>
+                            <a href={nav.LINK} className="nav-item">{nav.NAME}</a>
                         </li>)}
                     </ul>
                 </nav>
