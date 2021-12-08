c749877d401a0f59f44daeee01845a54d10170d7
mjkang0987 <snowdrop1202@gmail.com>
2021-12-08 12:31:47 +0900

    feat: 프로젝트 설정

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import {GlobalStyles} from './styles/GlobalStyles';
+
+export const App = () => {
+    return (
+        <>
+            <GlobalStyles/>
+            <div className="App">
+                SPA
+            </div>
+        </>
+    );
+};
---
+++ client/src/styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset} 
+  
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }`;
