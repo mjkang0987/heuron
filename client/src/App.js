017caac6f6ba1a1b624d70480335db64a3193aab
mjkang0987 <snowdrop1202@gmail.com>
2021-12-08 17:27:30 +0900

    test: swiper 마크업 테스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import {GlobalStyles} from './styles/GlobalStyles';
 
+import {Visual} from './components/Visual';
+
 export const App = () => {
     return (
         <>
             <GlobalStyles/>
-            <div className="App">
-                SPA
-            </div>
+            <Visual/>
         </>
     );
 };
