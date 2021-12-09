92853472f618773bf02d250ba3b7eb0414f3dd39
Minjeong kang <snowdrop1202@gmail.com>
2021-12-09 15:21:47 +0900

    feat: 비주얼 슬라이드 영역 마크업, 스타일 추가

--- client/src/components/Visual.js
+++ client/src/components/Visual.js
@@ -1,18 +1,41 @@
-// Import Swiper React components
-import {Swiper, SwiperSlide} from 'swiper/react';
+import {useEffect, useState} from 'react';
 
+import {Swiper, SwiperSlide} from 'swiper/react/swiper-react.js';
+import {Navigation, Pagination, Autoplay} from 'swiper';
 
-export const Visual = () => {
+import '../styles/visual.scss';
+
+export const Visual = ({data}) => {
+    const [thisSwiper, setThisSwiper] = useState(null);
+
+    useEffect(_ => {
+    }, [thisSwiper, setThisSwiper]);
+
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}>
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-        </Swiper>
+        <div className="visual">
+            <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                navigation
+                pagination={{clickable: true}}
+                autoplay={{delay: 4000}}
+                spaceBetween={50}
+                slidesPerView={3}
+                centeredSlides={true}
+                lazy={true}
+                onSwiper={(swiper) => setThisSwiper(swiper)}
+                className="visual-slides">
+                {data && data.slice(0, 10).map((slide, i) => <SwiperSlide
+                    key={`slide-${i}`}
+                    className="slide">
+                    <a href={slide.LINK}>
+                        <strong className="slide-title">{slide.author}</strong>
+                        <img
+                            src={slide.download_url}
+                            alt=""/>
+                    </a>
+                    <div className="visual-title">SLIDE DESCRIPTION</div>
+                </SwiperSlide>)}
+            </Swiper>
+        </div>
     );
 };
