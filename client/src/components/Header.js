71299cea1599c3f2b8aa74468d1ab5ae6eec1d8e
Minjeong kang <snowdrop1202@gmail.com>
2021-12-10 23:49:36 +0900

    feat: 인터섹션 옵저버 활용한 헤더 고정 분기

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,51 @@
+import {useCallback, useRef, useState} from 'react';
 import {Link} from 'react-router-dom';
+
+import {useIntersection} from '../hooks/useIntersection';
 
 import {NAV} from '../libs/constants/constants';
 
 import '../styles/header.scss';
 
 export const Header = () => {
+    const [fixed, setFixed] = useState({isFixed: false});
+    const targetRef = useRef(null);
+
+    const getFixed = ({isFixed}) => {
+        setFixed({
+            ...fixed,
+            isFixed: isFixed
+        });
+    };
+
+    const onIntersect = useCallback(
+        ([{isIntersecting}]) => {
+            getFixed({isFixed: isIntersecting});
+        }, [setFixed],
+    );
+
+    const {setTarget} = useIntersection({
+        target   : targetRef.current,
+        onIntersect,
+        threshold: 0
+    });
+
     return (
-        <header>
+        <header
+            ref={setTarget}
+            className={!fixed.isFixed ? 'fixed' : ''}>
             <div className="header-wrap">
                 <span className="logo">
-                    <Link to="#" className="link-home">로고</Link>
+                    <Link
+                        to="#"
+                        className="link-home">로고</Link>
                 </span>
                 <nav className="nav">
                     <ul>
                         {NAV.map((nav, index) => <li key={`nav-${index}`}>
-                            <Link to={nav.LINK} className="nav-item">{nav.NAME}</Link>
+                            <Link
+                                to={nav.LINK}
+                                className="nav-item">{nav.NAME}</Link>
                         </li>)}
                     </ul>
                 </nav>
