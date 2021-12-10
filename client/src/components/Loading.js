feb433c0b6c0bdc32dbb884e719d88d5aa0f6b10
Minjeong kang <snowdrop1202@gmail.com>
2021-12-11 00:23:08 +0900

    feat: 로딩 아이콘 추가

---
+++ client/src/components/Loading.js
@@ -0,0 +1,8 @@
+export const Loading = () => {
+    return (
+        <div className="loading">
+            <span className="icon-loading"/>
+            <span className="loading-text">LOADING</span>
+        </div>
+    );
+};
