367544e8e117985d4abc1cbb387a66f61de8d119
Minjeong kang <snowdrop1202@gmail.com>
2021-12-11 01:51:26 +0900

    chore: 사용하지 않는 useEffect 임포트 삭제

--- client/src/components/Visual.js
+++ client/src/components/Visual.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import {useRef, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 import {Swiper, SwiperSlide} from 'swiper/react/swiper-react.js';
