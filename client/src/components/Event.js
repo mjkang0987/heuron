553892c1b26d0a85a84b06c8133b1cd587d26576
Minjeong kang <snowdrop1202@gmail.com>
2021-12-11 01:45:05 +0900

    feat: 이벤트 영역 이미지 레이지로드 추가

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,19 +1,23 @@
+import {useRef} from 'react';
 import {Link} from 'react-router-dom';
 
 import {EVENT} from '../libs/constants/constants';
 
 import '../styles/event.scss';
+import {LazyImage} from './LazyImage';
 
 export const Event = ({data}) => {
+    const targetRefs = useRef([]);
+
     return (<div className="event">
             {data.map((event, index) => <div
                 key={`event-${index}`}
                 className="event-item">
                 <Link to="#">
                     <span className="img-wrap">
-                        <img
-                            src={event.download_url}
-                            alt=""/>
+                        <LazyImage
+                            ref={el => targetRefs.current[index] = el}
+                            source={event.download_url}/>
                     </span>
                     <strong className="event-title">{EVENT[index].TITLE}</strong>
                     <p className="event-desc">{EVENT[index].DESC}</p>
