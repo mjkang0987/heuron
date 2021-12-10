f63c76837fcf6f11e197de63fc5851f04f2b5c95
Minjeong kang <snowdrop1202@gmail.com>
2021-12-11 01:54:23 +0900

    refactor: 그리드 영역 이미지 레이지로드 추가

--- client/src/components/Masonry.js
+++ client/src/components/Masonry.js
@@ -1,11 +1,16 @@
+import {useRef} from 'react';
 import {Link} from 'react-router-dom';
 
 import {Portal} from './Portals';
 import {Layer} from './Layer';
 
+import {LazyImage} from './LazyImage';
+
 import '../styles/masonry.scss';
 
 export const Masonry = ({data}) => {
+    const targetRefs = useRef([]);
+
     return (<div className="masonry">
             <div className="items left">
                 {data.map((item, index) => <div
@@ -13,8 +18,9 @@ export const Masonry = ({data}) => {
                     className="item">
                     <div className="img-wrap">
                         <Link to="#">
-                            <img src={item.download_url}
-                                 alt={item.author}/>
+                            <LazyImage
+                                ref={el => targetRefs.current[index] = el}
+                                source={item.download_url}/>
                         </Link>
                     </div>
                 </div>)}
