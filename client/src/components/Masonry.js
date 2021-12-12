9abd6f43e7e6da7e979098713c1078b11e734ac0
Minjeong kang <snowdrop1202@gmail.com>
2021-12-12 18:01:13 +0900

    refactor: 그리드 영역 그리드 좌/우 스타일 부모 컴포넌트로 이관

--- client/src/components/Masonry.js
+++ client/src/components/Masonry.js
@@ -1,18 +1,42 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 
 import {MasonryItems} from './MasonryItems';
 
 import {GRID_LENGTH} from '../libs/constants/constants';
 
 import '../styles/masonry.scss';
+import {useIntersection} from '../hooks/useIntersection';
 
 export const Masonry = ({data}) => {
     const [pages, setPages] = useState(1);
 
-    return (<MasonryItems
-            items={data.slice(0, (pages * GRID_LENGTH > data.length ? data.length : pages * GRID_LENGTH))}
-            max={data.length}
-            pages={pages}
-            setPages={setPages}/>
+    const targetRef = useRef(null);
+
+    const onIntersect = ([{isIntersecting}]) => {
+        if (data.length === data.slice(0, (pages * GRID_LENGTH > data.length ? data.length : pages * GRID_LENGTH)).length) {
+            return;
+        }
+
+        if (isIntersecting) {
+            setPages(pages + 1);
+        }
+    };
+
+    const {setTarget} = useIntersection({
+        target   : targetRef.current,
+        onIntersect,
+        threshold: 1
+    });
+
+    return (<div className="masonry">
+            {Array(pages).fill(0).map((_, index) => <MasonryItems
+                key={`pages-${index}`}
+                items={data.slice(0, (pages * GRID_LENGTH > data.length ? data.length : pages * GRID_LENGTH))}
+                index={index}/>
+            )}
+            <div
+                ref={setTarget}
+                className="masonry-point"/>
+        </div>
     );
 };
--- client/src/components/MasonryItems.js
+++ client/src/components/MasonryItems.js
@@ -1,7 +1,5 @@
 import {useRef, useState} from 'react';
 
-import {useIntersection} from '../hooks/useIntersection';
-
 import {GRID_LENGTH} from '../libs/constants/constants';
 
 import {LazyImage} from './LazyImage';
@@ -9,9 +7,8 @@ import {LazyImage} from './LazyImage';
 import {Portal} from './Portals';
 import {Layer} from './Layer';
 
-export const MasonryItems = ({items, max, pages, setPages}) => {
+export const MasonryItems = ({items, index}) => {
     const targetRefs = useRef([]);
-    const targetRef = useRef(null);
 
     const [open, setOpen] = useState(false);
     const [animate, setAnimate] = useState(false);
@@ -34,43 +31,22 @@ export const MasonryItems = ({items, max, pages, setPages}) => {
         }
     };
 
-    const onIntersect = ([{isIntersecting}]) => {
-        if (max === items.length) {
-            return;
-        }
-
-        if (isIntersecting) {
-            setPages(pages + 1);
-        }
-    };
-
-    const {setTarget} = useIntersection({
-        target   : targetRef.current,
-        onIntersect,
-        threshold: 1
-    });
-
-    return (<div className="masonry">
-            {Array(pages).fill(0).map((_, index) => <div
-                key={`page-${index}`}
-                className={`items ${index % 2 === 0 ? 'left' : 'right'}`}>
-                {items.slice(index * GRID_LENGTH, (index + 1) * GRID_LENGTH).map((item, index) => <div
-                    key={`'item-${item.id}`}
-                    className="item">
-                    <div className="img-wrap">
-                        <button
-                            type="button"
-                            onClick={() => onOpen({source: item.download_url})}>
-                            <LazyImage
-                                ref={el => targetRefs.current[index] = el}
-                                source={item.download_url}/>
-                        </button>
-                    </div>
-                </div>)}
+    return (<div
+            className={`items ${index % 2 === 0 ? 'left' : 'right'}`}>
+            {items.slice(index * GRID_LENGTH, (index + 1) * GRID_LENGTH).map((item, index) => <div
+                key={`'item-${item.id}`}
+                className="item">
+                <div className="img-wrap">
+                    <button
+                        type="button"
+                        onClick={() => onOpen({source: item.download_url})}>
+                        <LazyImage
+                            ref={el => targetRefs.current[index] = el}
+                            source={item.download_url}/>
+                    </button>
+                </div>
             </div>)}
-            <div
-                ref={setTarget}
-                className="masonry-point"/>
+
             {open && <Portal>
                 <Layer
                     isOpen={open}
